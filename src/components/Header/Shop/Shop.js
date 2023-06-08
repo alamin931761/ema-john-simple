fa85a0d367c4c57c18bc9e97e4e552adb3815965
Al Amin <alamin931761@gmail.com>
2023-06-09 07:21:25 +0800

    card

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
+import Product from '../../Product/Product';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h2>This is for products</h2>
+                {
+                    products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
+                }
             </div>
 
             <div className="cart-container">
---
+++ src/components/Product/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Product.css';
+import { BsFillCartPlusFill } from 'react-icons/bs';
+
+const Product = ({ product, handleAddToCart }) => {
+    const { name, img, seller, price, ratings } = product;
+
+    return (
+        <div className='product'>
+            <img src={img} alt={name} />
+
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+                <p className='product-price'>Price: ${price}</p>
+                <p className='product-manufacturer'>Manufacturer : {seller}</p>
+                <p className='product-rating'>Rating : {ratings}</p>
+            </div>
+
+            <button onClick={() => handleAddToCart(product)} className='add-to-cart-button'>Add to Cart <BsFillCartPlusFill /></button>
+        </div>
+    );
+};
+
+export default Product;
